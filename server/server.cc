ee2ce9521f5e0627402bf321412250ef16f917d1
Grzegorz Miebs <grzegorz.miebs@o2.pl>
2017-01-25 10:26:45 +0100

    sended full messages with 'e' at end

--- server/server.cc
+++ server/server.cc
@@ -96,6 +96,8 @@ char* listToAttack() {
 
 void *addResources(void *threadID) {
     while (1) {
+        if (endGame == 2)
+            break;
         for (int i = 0; i < MAX_GAMER; i++) {
             if (players[i] >= 0) {
                 wood[i] += woodSpeed[i];
@@ -118,7 +120,7 @@ void theEnd(int pl) {
         int l = sprintf(buff, "WINNER%de", pl);
         for (int i = 0; i < MAX_GAMER; i++) {
             if (players[i] >= 0)
-                write(players[i], buff, l-1);
+                write(players[i], buff, l+1);
         }
         endGame = 2;
     }
@@ -175,7 +177,7 @@ void *sendAttack(void *args) {
             theEnd(attacker);
         }
         l = sprintf(buff, "h%d %d %d %d %de", archer[attacker], spear[attacker], points[attacker], wood[attacker], food[attacker]);
-        write(players[attacker], buff, l-1);
+        write(players[attacker], buff, l+1);
     } else {
         archer[target] -= archer[target] * deff/attack;
         spear[target] -= spear[target] * deff/attack;
@@ -187,6 +189,8 @@ void *sendAttack(void *args) {
 void *acceptAndInicializeGamer(void *threadID){
     nTmp = sizeof(struct sockaddr);
     while (1) {
+        if (endGame == 2)
+            break;
         nClientSocket = accept(nSocket, (struct sockaddr*) &stClientAddr, &nTmp);
         sleep(2);
         char buff[30];
@@ -226,7 +230,15 @@ void clear() {
             close(players[i]);
     }
     close(nSocket);
-    
+    free(wood);
+    free(food);
+    free(points);
+    free(archer);
+    free(spear);
+    free(players);
+    free(woodSpeed);
+    free(foodSpeed);
+    free(recrutationSpeed);
 }
 
 char* upgrade(int pl, char t) {
@@ -289,102 +301,103 @@ void recruit(int pl, char t, int x) {
             }
             break;
     }
-    write(players[pl], buff, l);
+    write(players[pl], buff, l+1);
 }
 
 int main(int argc, char* argv[]){
-   inicialize();
-   /* address structure */
-   memset(&stAddr, 0, sizeof(struct sockaddr));
-   stAddr.sin_family = AF_INET;
-   stAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   stAddr.sin_port = htons(SERVER_PORT);
+    while (1) {
+        inicialize();
+        /* address structure */
+        memset(&stAddr, 0, sizeof(struct sockaddr));
+        stAddr.sin_family = AF_INET;
+        stAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        stAddr.sin_port = htons(SERVER_PORT);
 
-   /* create a socket */
-   nSocket = socket(AF_INET, SOCK_STREAM, 0);
-   if (nSocket < 0) {
-       fprintf(stderr, "%s: Can't create a socket.\n", argv[0]);
-       exit(1);
-   }
-   setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&nFoo, sizeof(nFoo));
+        /* create a socket */
+        nSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (nSocket < 0) {
+            fprintf(stderr, "%s: Can't create a socket.\n", argv[0]);
+            exit(1);
+        }
+        setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&nFoo, sizeof(nFoo));
 
-   /* bind a name to a socket */
-   nBind = bind(nSocket, (struct sockaddr*)&stAddr, sizeof(struct sockaddr));
-   if (nBind < 0){
-       fprintf(stderr, "%s: Can't bind a name to a socket.\n", argv[0]);
-       exit(1);
-   }
-   /* specify queue size */
-   nListen = listen(nSocket, QUEUE_SIZE);
-   if (nListen < 0){
-       fprintf(stderr, "%s: Can't set queue size.\n", argv[0]);
-   }
+        /* bind a name to a socket */
+        nBind = bind(nSocket, (struct sockaddr*)&stAddr, sizeof(struct sockaddr));
+        if (nBind < 0){
+            fprintf(stderr, "%s: Can't bind a name to a socket.\n", argv[0]);
+            exit(1);
+        }
+        /* specify queue size */
+        nListen = listen(nSocket, QUEUE_SIZE);
+        if (nListen < 0){
+            fprintf(stderr, "%s: Can't set queue size.\n", argv[0]);
+        }
 
-   pthread_t thread[2];
-   long t = 1;
-   printf("%d\n", nSocket);
-   if (pthread_create(&thread[0], NULL, acceptAndInicializeGamer, (void *)t) < 0)
-       printf("ERROR \n");
-   t++;
-   if (pthread_create(&thread[1], NULL, addResources, (void *)t) < 0)
-       printf("ERROR \n");
-   while (1) {
-       if (endGame == 2) {
-           endGame = 0;
-           break;
-       }
-       epoll_wait(epollDesc, &event, 1, -1);
-       for (unsigned int pl = 0; pl < MAX_GAMER; pl++) {
-           if (players[pl] != -1 && event.data.u32 == (pl+1) * 1000) {
-                printf("button: %d\n", pl);
-                char buff[255], helper[2];
-                memset(buff, 0, 255);
-                buff[0] = '\0';
-                printf("%s\n", buff);
-                int l;
-                l = read(players[pl], buff, 1);
-                if (l < 1)
-                    closePlayer(pl);
-                else {
-                    helper[0] = '0';
-                    while (helper[0] != 'e') {
-                        l = read(players[pl], helper, 1);
-                        if (l>0) strcat(buff, helper);
-                    }
-                    char message[255], *s;
-                    int l;
+        pthread_t thread[2];
+        long t = 1;
+        printf("%d\n", nSocket);
+        if (pthread_create(&thread[0], NULL, acceptAndInicializeGamer, (void *)t) < 0)
+            printf("ERROR \n");
+        t++;
+        if (pthread_create(&thread[1], NULL, addResources, (void *)t) < 0)
+            printf("ERROR \n");
+        while (1) {
+            if (endGame == 2) {
+                break;
+            }
+            epoll_wait(epollDesc, &event, 1, -1);
+            for (unsigned int pl = 0; pl < MAX_GAMER; pl++) {
+                if (players[pl] != -1 && event.data.u32 == (pl+1) * 1000) {
+                    printf("button: %d\n", pl);
+                    char buff[255], helper[2];
+                    memset(buff, 0, 255);
+                    buff[0] = '\0';
                     printf("%s\n", buff);
-                    event.data.u32 = 0;
-                    switch (buff[0]) {
-                        case 'u': // upgrade
-                            l = sprintf(message, "u%c%se", buff[1], upgrade(pl, buff[1]));
-                            write(players[pl], message, l+1);
-                            break;
-                        case 'a': // want list of villages to attack
-                            l = sprintf(message, "l %s", listToAttack());
-                            write(players[pl], message, l+1);
-                            break;
-                        case 's' : // send attack
-                            l = strlen(buff);
-                            s = (char*)malloc((l-1)*sizeof(char));
-                            strncpy(s, buff+1, l-2);
-                            s[l-2] = '\0';
-                            pthread_t a;
-                            pthread_create(&a, NULL, sendAttack, (void *)s);
-                            break;
-                        case 'r' : // recruit
-                            int l = strlen(buff);
-                            s = (char*)malloc((l-2)*sizeof(char));
-                            strncpy(s, buff+2,l-3);
-                            sscanf(s, "%d", &l);
-                            recruit(pl, buff[1], l);
-
-                            break;
+                    int l;
+                    l = read(players[pl], buff, 1);
+                    if (l < 1)
+                        closePlayer(pl);
+                    else {
+                        helper[0] = '0';
+                        while (helper[0] != 'e') {
+                            l = read(players[pl], helper, 1);
+                            if (l>0) strcat(buff, helper);
+                        }
+                        char message[255], *s;
+                        int l;
+                        printf("%s\n", buff);
+                        event.data.u32 = 0;
+                        switch (buff[0]) {
+                            case 'u': // upgrade
+                                l = sprintf(message, "u%c%se", buff[1], upgrade(pl, buff[1]));
+                                write(players[pl], message, l+1);
+                                break;
+                            case 'a': // want list of villages to attack
+                                l = sprintf(message, "l %s", listToAttack());
+                                write(players[pl], message, l+1);
+                                printf("%s\n", message);
+                                break;
+                            case 's' : // send attack
+                                l = strlen(buff);
+                                s = (char*)malloc((l-1)*sizeof(char));
+                                strncpy(s, buff+1, l-2);
+                                s[l-2] = '\0';
+                                pthread_t a;
+                                pthread_create(&a, NULL, sendAttack, (void *)s);
+                                break;
+                            case 'r' : // recruit
+                                int l = strlen(buff);
+                                s = (char*)malloc((l-2)*sizeof(char));
+                                strncpy(s, buff+2,l-3);
+                                sscanf(s, "%d", &l);
+                                recruit(pl, buff[1], l);
+                                break;
+                        }
                     }
                 }
-           }
-       }
-   }
-   clear();
-   return(0);
+            }
+        }
+        clear();
+    }
+    return(0);
 }
